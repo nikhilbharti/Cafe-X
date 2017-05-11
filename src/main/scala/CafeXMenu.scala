f9db3dac8b725ac10b2f332a10efa2e4965fde39
nikhil.bharti <nikhil.bharti@digital.hmrc.gov.uk>
2017-05-11 22:34:59 +0100

    Step 2: Service Charge
    Add support for a service charge :
    
    ● When all purchased items are drinks no service charge is applied
    
    ● When purchased items include any food apply a service charge of 10% to the total bill
    
    (rounded to 2 decimal places)
    
    ● When purchased items include any hot food apply a service charge of 20% to the total bill
    
    with a maximum £20 service charge

--- src/main/scala/CafeXMenu.scala
+++ src/main/scala/CafeXMenu.scala
@@ -5,14 +5,43 @@ sealed trait Item {
   val price: Double
 }
 
-case class Cola(price: Double = 0.5) extends Item
-case class Coffee(price: Double = 1) extends Item
-case class CheeseSandwich(price: Double = 2) extends Item
-case class SteakSandwich(price: Double = 4.5) extends Item
+sealed trait Drinks extends Item
+sealed trait Food extends Item
+sealed trait HotFood extends Item
+
+case class Cola(price: Double = 0.5) extends Drinks
+case class Coffee(price: Double = 1) extends Drinks
+case class CheeseSandwich(price: Double = 2) extends Food
+case class SteakSandwich(price: Double = 4.5) extends HotFood
+
 
 
 object CafeXMenu {
 
+ val FOOD_SERVICE_CHARGE = 0.1 // 10%
+ val HOT_FOOD_SERVICE_CHARGE = 0.2 // 20%
+  val MAX_SERVICE_CHARGE = 20
+
   def calculateBill(items: Seq[Item]): Double = items.foldLeft(0.0)(_ + _.price)
+
+  def calculateBillWithCharges(items: Seq[Item]): Double = {
+    val purchaseOnlyDrinks = items.forall(x => x.isInstanceOf[Drinks])
+    val purchaseContainFood = items.exists(x => x.isInstanceOf[Food])
+    val purchaseContainHotFood = items.exists(x => x.isInstanceOf[HotFood])
+
+    (purchaseOnlyDrinks,purchaseContainFood,purchaseContainHotFood) match {
+      case (true,_ ,_)  => calculateBill(items)
+      case (_,true,_) => round2Decimal(calculateBill(items) + (calculateBill(items)* FOOD_SERVICE_CHARGE))
+      case (_,_,true) => if(calculateBill(items)* HOT_FOOD_SERVICE_CHARGE < 20)
+        round2Decimal(calculateBill(items) + (calculateBill(items)* HOT_FOOD_SERVICE_CHARGE))
+        else
+        round2Decimal(calculateBill(items) + MAX_SERVICE_CHARGE)
+      case (_,_,_) => 0.0
+    }
+  }
+
+  def round2Decimal(value : Double) = {
+    BigDecimal(value).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  }
 
 }
--- src/test/scala/CafeXMenuSpec.scala
+++ src/test/scala/CafeXMenuSpec.scala
@@ -13,4 +13,26 @@ class CafeXMenuSpec extends FlatSpec with Matchers {
   it should "return 0 for an empty list of items" in {
     calculateBill(List.empty) should be(0)
   }
+
+  "calculateBillWithCharges" should "NOT apply a service charge when only drinks are purchased" in {
+    calculateBillWithCharges(List(Cola(), Coffee())) should be (1.5)
+  }
+
+  "calculateBillWithCharges" should "apply a service charge of 10% when purchased item includes any FOOD" in {
+    calculateBillWithCharges(List(Cola(), Coffee(),CheeseSandwich())) should be (3.85)
+  }
+
+  "calculateBillWithCharges" should "apply a service charge of 20% when purchased items includes any HOT FOOD" in {
+    calculateBillWithCharges(List(Cola(), Coffee(),SteakSandwich())) should be (7.2)
+  }
+
+  "Calculated bill" should "round to 2 decimals" in {
+    round2Decimal(2.866674) should be (2.87)
+  }
+
+  "calculateBillWithCharges" should "apply a service charge of 20% to the total bill with a maximum £20 service charge" +
+    "when purchased items include any hot food" in {
+    calculateBillWithCharges(List(SteakSandwich(120))) should be (140)
+  }
+
 }
